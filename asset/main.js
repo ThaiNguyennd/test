ff6a150b09c8045ab912e72ead696ec5903c0e08
ThaiNguyennd <thaittn01@gmail.com>
2021-12-07 20:59:12 +0700

    add

---
+++ asset/main.js
@@ -0,0 +1,11 @@
+let talkAnswer = document.getElementsByClassName("talkAnswer");
+let eatAnsswer = document.getElementsByClassName("eatAnswer");
+let feelAnswer = document.getElementsByClassName("fellAnswer");
+let btnDetermine = document.getElementsByClassName("btnDetermine");
+let imgcontainer = document.getElementsByClassName("imgcontainer");
+
+btnDetermine.addEventListener("clicl", hamgido);
+
+function hamgido(){
+    
+}
