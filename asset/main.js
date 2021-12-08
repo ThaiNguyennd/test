0740f91a4cc4dab9554de7b9bd946312a0ff2e4c
ThaiNguyennd <thaittn01@gmail.com>
2021-12-08 23:14:50 +0700

    upload

--- asset/main.js
+++ asset/main.js
@@ -1,11 +1,34 @@
-let talkAnswer = document.getElementsByClassName("talkAnswer");
-let eatAnsswer = document.getElementsByClassName("eatAnswer");
-let feelAnswer = document.getElementsByClassName("fellAnswer");
-let btnDetermine = document.getElementsByClassName("btnDetermine");
-let imgcontainer = document.getElementsByClassName("imgcontainer");
+// let talkAnswer = document.getElementById("talkAnswer");
+// let eatAnsswer = document.getElementById("eatAnswer");
+// let feelAnswer = document.getElementById("fellAnswer");
+// // let btnDetermine = document.getElementsByClassName("btnDetermine");
+// let imgcontainer = document.getElementById("imgcontainer");
 
-btnDetermine.addEventListener("clicl", hamgido);
+// // btnDetermine.addEventListener("click", hamgido);
 
-function hamgido(){
-    
-}
+// function hamgido(){
+//     if(talkAnswer.value=="1a" && eatAnsswer.value == "2c" && feelAnswer.value=="3a" ){
+//         // let title = document.getElementsByClassName("title");
+//         // title.textContent=="You are an alien";
+//         // imgcontainer.setAttribute= ('src','asset/img/alien.jpg')
+//         document.getElementsById("title").innerHTML="you're an alien";
+//         imgcontainer.src = "./img/alien.jpg";
+
+//     }
+// }
+function hamgido() {
+    var a = document.getElementById("talkAnswer");
+    var b = document.getElementById("eatAnswer");
+    var c = document.getElementById("fellAnswer");
+  
+    if (a.value == "1a" && b.value == "2c" && c.value == "3a") {
+      document.getElementById("title").innerHTML = "You're an alien";
+      document.getElementById("imgcontainer").src = "./img/alien.jpg";
+    } else if (a.value == "1b" && b.value == "2a" && c.value == "3a") {
+      document.getElementById("title").innerHTML = "You're an Bizarre";
+      document.getElementById("imgcontainer").src = "./img/bizarre.jpg";
+    } else {
+      document.getElementById("title").innerHTML = "You're an Human";
+      document.getElementById("imgcontainer").src = "./img/normal.jpg";
+    }
+  }
